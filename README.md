e80a47fe31853b81df50f4a196d5e02326f0a128
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-05-03 21:10:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**list**: https://ibb.co/VgwGymB
+
+**get**: https://ibb.co/wwZb8bk
+
+**add**: https://ibb.co/HXmrjsX
+
+**remove**: https://ibb.co/gV5HyNc
---
+++ db/contacts.js
@@ -0,0 +1,45 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const newContacts = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
